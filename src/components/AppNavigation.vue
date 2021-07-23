137aa8a3d8839bc0643bd122090a3c4b84ab6229
Krai Remle Alapide <krai.alapide@gmail.com>
2021-07-23 22:27:22 +0800

    Updates on different pages

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -19,19 +19,23 @@
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="brown darken-4" dark>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-spacer class="d-flex d-md-none"></v-spacer>
-            <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-            <v-btn flat class="d-none d-md-flex">Menu</v-btn>
-            <v-spacer class="d-none d-md-flex"></v-spacer>
-            <v-btn flat class="d-none d-md-flex">SIGN IN</v-btn>
-            <v-btn color="brown lighten-3" class="d-none d-md-flex">
-                JOIN
-            </v-btn>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <router-link to="/">
+                <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
+            </router-link>
+            <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn flat class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join"
+                >JOIN</v-btn
+            >
         </v-toolbar>
     </span>
 </template>
-
 <script>
 export default {
     name: 'AppNavigation',
@@ -39,10 +43,14 @@ export default {
         return {
             appTitle: 'Meal Prep',
             drawer: false,
-            items: [{ title: 'Menu' }, { title: 'Sign In' }, { title: 'Join' }],
+            items: [
+                { title: 'Menu' },
+                { title: 'Profile' },
+                { title: 'Sign In' },
+                { title: 'Join' },
+            ],
         };
     },
 };
 </script>
-
 <style scoped></style>
